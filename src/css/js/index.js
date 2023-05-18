62d742e62c6e06fc78cca0df6c644ec8a126df35
Jojohshs <133508501+Jojohshs@users.noreply.github.com>
2023-05-18 20:49:26 -0300

    Add files via upload

--- src/css/js/index.js
+++ src/css/js/index.js
@@ -1,16 +1,17 @@
+
+
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
 const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
-botaoAlterarTema.addEventListener("click", () => {
-    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
-  
-    body.classList.toggle("modo-escuro");
-  
-    if (modoEscuroEstaAtivo) {
-      imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/Disgaea 1 logo.jpg");
-    } else {
-      imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/Disgaea_4_Logo.webp");
-    }
-  });
-  
 
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/Disgaea 1 logo.jpg");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/Disgaea 5 logo.jpg");
+  }
+});
